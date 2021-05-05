84ff566de97968e6b7650d0076fbbbd177837882
root <Busibo@inbox.ru>
2021-05-05 20:54:52 +0000

    yellow background

--- colorbg/colorbg.asm
+++ colorbg/colorbg.asm
@@ -8,8 +8,19 @@
 
 START:
 	CLEAN_START		; Macro to safely clear the memory
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Set background luminosity color to yellow
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+Loop_BG:
+	lda #$1E		; Load color into A ($1E is NTSC yellow)
+	sta COLUBK		; Store A to Background_Color Address $09
+	
+	jmp Loop_BG		; Repeat from START	
 
-
-	org $FFFC
-	.word START
-	.word START	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Fill ROM size to exactly 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+	org $FFFC		; Define origin to $FFFC
+	.word START		; Reset vector at $FFFC (where program starts)
+	.word START		; Interrupt vector at $FFFE (unused in the VCS)
